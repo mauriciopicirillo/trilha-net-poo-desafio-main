7e7d094f331e67a8be51701582485c015b421fd0
Mauricio Aparecido Picirillo <79844553+mauriciopicirillo@users.noreply.github.com>
2022-09-17 12:03:43 -0300

    final

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -3,11 +3,9 @@ using System;
 namespace DesafioPOO.Models
 {
 
-    // TODO: Herdar da classe "Smartphone"
     public class Nokia : Smartphone
     {
 
-        // TODO: Sobrescrever o método "InstalarAplicativo"
         public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
         {
         }
